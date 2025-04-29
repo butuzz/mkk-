fa835602c57ac8e9a42d923c6557efe8327b1eb6
butuzz <bombastick2009@gmail.com>
2025-04-29 22:36:26 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -14,4 +14,26 @@ const nextConfig = {
   },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = withPWA(nextConfig)
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  swcMinify: true,
+  pwa: {
+    dest: 'public', // Папка для хранения сервис-воркера
+    runtimeCaching: [
+      {
+        urlPattern: /^https:\/\/.*\.(js|css|html|json)$/,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'static-resources',
+          expiration: {
+            maxEntries: 100,
+            maxAgeSeconds: 60 * 60 * 24 * 30, // Кэшируем на 30 дней
+          },
+        },
+      },
+    ],
+  },
+});
