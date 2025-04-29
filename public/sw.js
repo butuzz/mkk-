5f18d67de5df08444a16b868a2a79b545f90e338
butuzz <bombastick2009@gmail.com>
2025-04-29 23:12:26 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -23,3 +23,27 @@ self.addEventListener('fetch', (e) => {
     })
   );
 });
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('chatgpt-app').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/favicon.ico',
+        '/icon.png',
+        '/static/js/bundle.js',
+        '/static/js/1.chunk.js',
+        '/static/js/main.chunk.js',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
