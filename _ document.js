68ddee88ae241dace4c77f1c9f8942030cb05f67
butuzz <bombastick2009@gmail.com>
2025-04-29 22:28:34 +0200

    Create _ document.js

---
+++ _ document.js
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* PWA Manifest */}
+          <link rel="manifest" href="/manifest.json" />
+          {/* Добавить иконки */}
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="theme-color" content="#000000" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
