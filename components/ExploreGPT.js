b8a1f050faed503ff7ebcf807b2c911df6461f30
butuzz <bombastick2009@gmail.com>
2025-04-29 21:55:51 +0200

    Create ExploreGPT.js

---
+++ components/ExploreGPT.js
@@ -0,0 +1,9 @@
+export default function ExploreGPT() {
+  return (
+    <div>
+      <h2>Explore GPT</h2>
+      <p>Here you can explore different GPT models and their capabilities!</p>
+      {/* Добавь больше функционала по своему желанию */}
+    </div>
+  );
+}
