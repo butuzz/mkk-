1ab17eb9674e9589ffcaf3069d1c9d1f1fa21b35
butuzz <bombastick2009@gmail.com>
2025-04-29 22:35:29 +0200

    Update _ document.js

--- _ document.js
+++ _ document.js
@@ -21,3 +21,32 @@ class MyDocument extends Document {
 }
 
 export default MyDocument;
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Подключаем PWA manifest */}
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="theme-color" content="#000000" />
+          <link rel="icon" href="/favicon.ico" />
+          {/* Подключаем иконки */}
+          <link
+            rel="icon"
+            href="/icon.png"
+            sizes="192x192"
+            type="image/png"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
