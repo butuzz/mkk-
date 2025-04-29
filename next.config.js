743358d2ae6a9195941f50577ee5fe99f96ce087
butuzz <bombastick2009@gmail.com>
2025-04-29 23:02:05 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -37,3 +37,13 @@ module.exports = withPWA({
     ],
   },
 });
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
