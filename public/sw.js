09febf60a4cb5493d3253dfed76b48f1cb0c8e19
butuzz <bombastick2009@gmail.com>
2025-04-30 06:37:41 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -47,3 +47,9 @@ self.addEventListener('fetch', (e) => {
     })
   );
 });
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('v1').then(c => c.addAll(['/', '/mkk-/', '/mkk-/index.html'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
